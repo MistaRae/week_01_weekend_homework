bb183fa5d48cf63aca4fd9b663227e00ff577567
MistaRae <itsamistarae@gmail.com>
2021-04-02 14:52:18 +0100

    second function defined and working

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,5 +1,11 @@
 # WRITE YOUR FUNCTIONS HERE
+# import pdb
 
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(income_or_expenditure)
+    balance = "total_cash"
