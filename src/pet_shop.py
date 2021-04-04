02d51f6f724bfd7536b54aba774aaa38d42df55e
MistaRae <itsamistarae@gmail.com>
2021-04-04 12:15:31 +0100

    5th and 6th functions defined & running

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,15 +1,21 @@
 # WRITE YOUR FUNCTIONS HERE
 # import pdb
 
-def get_pet_shop_name(pet_shop):
-    return pet_shop["name"]
+def get_pet_shop_name(cc_pet_shop):
+    return cc_pet_shop["name"]
 
-def get_total_cash(pet_shop):
-    return pet_shop["admin"]["total_cash"]
+def get_total_cash(cc_pet_shop):
+    return cc_pet_shop["admin"]["total_cash"]
 
-def add_or_remove_cash(pet_shop, income_or_expenditure):
+def add_or_remove_cash(cc_pet_shop, income_or_expenditure):
 #update the total cash value using a new argument
-    pet_shop["admin"]["total_cash"] += income_or_expenditure
+    cc_pet_shop["admin"]["total_cash"] += income_or_expenditure
     
-def get_pets_sold(pet_shop):
-    return pet_shop["admin"]["pets_sold"]
+def get_pets_sold(cc_pet_shop):
+    return cc_pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(cc_pet_shop, no_of_pets_change):
+    #update the pets sold value in admin dictionary using a new argument
+    cc_pet_shop["admin"]["pets_sold"] += no_of_pets_change
+
+# def get_stock_count(cc_pet_shop, )
